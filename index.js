6dec7819e1aa37d2216c0bd3ab853381820d2350
Arturo Blanco <blancoarturo92@gmail.com>
2022-08-12 23:42:53 -0300

    se corrije sintaxis mal echas

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
-let sumaTotal = 0 ;
+"use strict";
+let subTotal= 0;
+let sumaTotal=0;
+let botonAgregar=document.querySelector(".btnAgregar");
+let botonTotal=document.querySelector(".btnTotal");
+
+botonAgregar.addEventListener("click",calcularCompra);
+botonTotal.addEventListener("click",calcularTotal);
+
+function calcularCompra() {
+    let producto= document.querySelector(".product").value;
+    let cantidad= document.querySelector(".cant").value;
+    let costo= document.querySelector(".cost").value;
+    subTotal=(parseInt(costo)*parseInt(cantidad));
+    let elemProd=document.createElement("li");
+    let elemCant=document.createElement("li");
+    let elemCost=document.createElement("li");
+    let elemSubt=document.createElement("li");
+    let texProd=document.createTextNode(producto);
+    let texCant=document.createTextNode(cantidad);
+    let texCost=document.createTextNode(costo);
+    let texSub=document.createTextNode(subTotal);
+    elemProd.appendChild(texProd);
+    elemCant.appendChild(texCant);
+    elemCost.appendChild(texCost);
+    elemSubt.appendChild(texSub);
+    listProd.appendChild(elemProd);
+    listCant.appendChild(elemCant);
+    listCost.appendChild(elemCost);
+    listSub.appendChild(elemSubt);
+    sumaTotal= (parseInt(sumaTotal)+ parseInt(subTotal))
+    }
+
+function calcularTotal() {
+let total= sumaTotal;
+    document.querySelector(".total").innerHTML=`El costo total es: $ `+ total
+}
+
+/*let sumaTotal = 0 ;
 
 function funcAgreg() {
     let producto = document.getElementById("producto").value; 
@@ -17,4 +55,5 @@ function funcAgreg() {
 function funcTotal() {
     let total = sumaTotal ;
     document.getElementById("total").innerHTML = `El costo total es: $ ` + total;   
-}
+} */
+
